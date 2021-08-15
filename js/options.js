14dea9f7e6024ca82ff89aa6a3401f94690fd4d1
gmachine1 <gmachine1729@foxmail.com>
2021-08-15 19:49:02 +0800

    use hidden attribute instead of css, set all if no checkboxes are selected

--- js/options.js
+++ js/options.js
@@ -6,17 +6,28 @@ function loadOptions() {
   chrome.storage.local.get('lang_list', function(properties) {
     console.log('lang_list: ' + properties['lang_list'])
 
-    properties['lang_list'].forEach(function(value, index, array) {
+    var langList = properties['lang_list']
+    if (langList == undefined || langList.length == 0) {
+      langList = $("input[name='lang']").toArray().map(function getId(e) { return e.id} );
+    }
+    langList.forEach(function(value, index, array) {
       document.getElementById(value).checked = true;
     });
   })
-  $('body').css("visibility","visible");
+  $('body').attr("hidden", false);
 }
 
 function saveOptions() {
-  var langList = $("input[name='lang']:checked").toArray().map(function getId(e) { return e.id} )
+  var langList = $("input[name='lang']:checked").toArray().map(function getId(e) { return e.id} );
+  display_all_languages = langList.length == 0;
+  if (display_all_languages) {
+    langList = $("input[name='lang']").toArray().map(function getId(e) { return e.id} );
+    langList.forEach(function(value, index, array) {
+      document.getElementById(value).checked = true;
+    });
+  }
   chrome.storage.local.set({'lang_list': langList}, function() {
-      console.log('lang_list set to: ' + langList)
-    })
-  alert("Options saved!");
+    console.log('lang_list set to: ' + langList);
+  })
+  setTimeout(function() {  alert("Options saved!"); }, 100);
 }
